4c6cc1093db1fc899615424c7f379e993df9edd4
turchak <turchaksv@gmail.com>
2018-06-06 14:12:07 +0300

    add list component

---
+++ src/app/pages/list/components/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pok-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.sass']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
