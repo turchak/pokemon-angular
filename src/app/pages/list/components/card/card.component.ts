d028efb78bbceda83c5d09e931300adb3cab07fe
turchak <turchaksv@gmail.com>
2018-06-08 01:37:10 +0300

    add svg icons

--- src/app/pages/list/components/card/card.component.ts
+++ src/app/pages/list/components/card/card.component.ts
@@ -9,9 +9,32 @@ import { IPokemon } from '../../../../def/pokemon';
 export class CardComponent implements OnInit {
   @Input()
   pokemon: IPokemon
+
+  
   constructor() { }
+
+  normalizeId(n: number) {
+    let id;
+    const digits = n.toString().split("");
+
+    if (digits.length >= 3) {
+      return n;
+    }
+
+    if (digits.length === 2) {
+      digits.unshift('0');
+      return digits.join('')
+    }
+
+    if (digits.length === 1) {
+      id = digits.unshift('0', '0');
+      return digits.join('')
+    }
+  }
 
   ngOnInit() {
   }
+
+  
 
 }
